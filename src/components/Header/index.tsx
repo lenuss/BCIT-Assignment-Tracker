07b7dc461527e9cda67ed5471704b47fbb6b89a7
Lenka <lenka.bistakova@gmail.com>
2025-06-09 20:16:51 -0700

    disabled button & state in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,35 @@
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
+import { useState } from "react";
 
 export function Header() {
+  const [newAssignment, setNewAssignment] = useState("");
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewAssignment(event.target.value);
+  }
+
   return (
     <header className={styles.header}>
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
-      <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" />
-        <button>
+      <form onSubmit={handleSubmit} className={styles.newAssignmentForm}>
+        <input
+          value={newAssignment}
+          onChange={handleChange}
+          placeholder="Add a new assignment"
+          type="text"
+        />
+        <button
+          disabled={newAssignment === ""}
+          type="submit"
+          className=""
+        >
           Create <AiOutlinePlusCircle size={20} />
         </button>
       </form>
